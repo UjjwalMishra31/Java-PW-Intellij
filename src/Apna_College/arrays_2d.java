7406cdc3870b2ca47f3b75d82f91359bb1adc524
Ujjwal Mishra <ujjwalmishra3131@gmail.com>
2025-01-24 22:53:09 +0530

    first commit and basics on java with pw

---
+++ src/Apna_College/arrays_2d.java
@@ -0,0 +1,13 @@
+package Apna_College;
+
+public class arrays_2d
+{
+    public static void main(String[] args)
+    {
+        int[][] studentmarks = {{78,56,97},{77,99,45},{64,78,56}};
+        // for decalring 2d arrays use two [] brackets
+        System.out.println(studentmarks[0][1]);//student 1 marks 1
+        System.out.println(studentmarks[1][1]);//student 2 marks 1
+        System.out.println(studentmarks[2][1]);//student 3 marks 1
+    }
+}
--- src/Apna_College/arrays_java.java
+++ src/Apna_College/arrays_java.java
@@ -1,9 +1,27 @@
 package Apna_College;
 
+import java.util.Arrays;
+
 public class arrays_java
 {
     public static void main(String[] args)
     {
-
+        int[] marks = new int[5];
+        marks[0]=10;
+        marks[1]= 2;
+        marks[2]=11;
+        marks[3]= 21;
+        marks[4]= 16;
+        System.out.println(marks[2]);
+        System.out.println(marks[4]);
+        // length of arrays
+        System.out.println(marks.length);
+        // sorting of arrays
+        Arrays.sort(marks);
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        System.out.println(marks[3]);
+        System.out.println(marks[4]);
     }
 }
